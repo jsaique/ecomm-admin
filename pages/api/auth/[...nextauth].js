0d7e1c986e18a91e4b19b58d5f505ad1a0fc2406
jsaique <jsaique11@gmail.com>
2023-10-22 11:33:31 -0400

    Next auth and MongoDB

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,6 +3,8 @@ import NextAuth from "next-auth";
 // import FacebookProvider from 'next-auth/providers/facebook'
 import GoogleProvider from "next-auth/providers/google";
 // import EmailProvider from "next-auth/providers/email";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "@/lib/mongodb";
 
 export default NextAuth({
   providers: [
@@ -25,4 +27,5 @@ export default NextAuth({
     //   from: "NextAuth.js <no-reply@example.com>",
     // }),
   ],
+  adapter: MongoDBAdapter(clientPromise),
 });
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   const { data: session } = useSession();
   if (!session) {
     return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
+      <div className="bg-gradient-to-b from-teal-500 w-screen h-screen flex items-center">
         <div className="text-center w-full">
           <button
             onClick={() => signIn("google")}
@@ -16,5 +16,9 @@ export default function Home() {
       </div>
     );
   }
-  return <div>Logged in as {session.user.email}</div>;
+  return (
+    <div className="bg-gradient-to-b from-teal-500 min-h-screen">
+      <div>Logged in as {session.user.email}</div>
+    </div>
+  );
 }
